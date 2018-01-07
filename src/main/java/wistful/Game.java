869158e41781659945e046004342ab019fec4ce0
risx <rrrisx@gmail.com>
2018-01-06 20:47:33 -0800

    Added movement params

--- src/main/java/wistful/Game.java
+++ src/main/java/wistful/Game.java
@@ -7,15 +7,21 @@ public class Game {
     public final Integer id; //id for mapping other games
 
     private Board board;
-    public Integer[] selector_position = {1, 1};
+    public Integer[] selector_position;
     public Integer score;
+    public Integer active_time;
+    public Integer pause_time;
+    public Integer move;
 
-    public Game(Integer id){
+    public Game(Integer id) {
         UUID uuid = UUID.randomUUID();
         String randomUUIDString = uuid.toString();
 
         this.gid = randomUUIDString;
         this.score = 0;
+        this.active_time = 0;
+        this.pause_time = 0;
+        this.move = 0;
         this.id = id;
         this.board = new Board();
     }
--- src/main/java/wistful/GameController.java
+++ src/main/java/wistful/GameController.java
@@ -1,24 +1,34 @@
 package wistful;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
 public class GameController {
-    HashMap<Integer, Game> existantgames = new HashMap<Integer, Game>();
+    HashMap<Integer, Game> existantgames = new HashMap<>();
 
     @RequestMapping("/")
-    public String index() { return "Oh, hey, I didn't see you there."; }
+    public String index() {
+        return "Oh, hey, I didn't see you there.";
+    }
 
-    @RequestMapping(value = "/ping", method = RequestMethod.GET, produces = "application/json")
-    public String ping() { return "Cya space cowboy!"; }
+    @RequestMapping(value = "/ping", method = RequestMethod.GET,
+            produces = "application/json")
+    public String ping() {
+        return "Cya space cowboy!";
+    }
 
-    @RequestMapping(value = "/g", method = RequestMethod.GET, produces = "application/json")
-    public Game game(@RequestParam(value="id", defaultValue="null") Integer id) {
+    @RequestMapping(value = "/g/{id}/{leftmove}/{rightmove}",
+            method = RequestMethod.GET, produces = "application/json")
+    //public Game game(@RequestParam(value="id", defaultValue="null") Integer id) {
+    public Game game(@PathVariable("id") Integer id,
+                     @PathVariable("leftmove") Integer leftmove,
+                     @PathVariable("rightmove") Integer rightmove){
+
+        Integer left = leftmove;
+        Integer right = rightmove;
+
         if (existantgames.get(id) == null){
             existantgames.put(id, new Game(id));
         }
